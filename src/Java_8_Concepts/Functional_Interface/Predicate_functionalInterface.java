407e5d3b4374df1237312b3222442f547006c38d
ThamizhArasanJS200357 <thamizharasan2003@gmail.com>
2025-09-04 14:42:52 +0530

    Predicate Programs

--- src/Java_8_Concepts/Functional_Interface/Predicate_functionalInterface.java
+++ src/Java_8_Concepts/Functional_Interface/Predicate_functionalInterface.java
@@ -8,11 +8,13 @@ public class Predicate_functionalInterface {
     public static void main(String[]args){
         List<String> st= Arrays.asList("JS","rolex","thamizh","arasan");
         Predicate<String> p=(s)->s.startsWith("r");
+        Predicate<Integer>even=(s)->s%2==0; // Even or Odd.
         for(String s:st){
             if(p.test(s)){
                 System.out.println(s);
             }
         }
+        System.out.println(even.test(12)?"Even":"Odd");
 
     }
 }
